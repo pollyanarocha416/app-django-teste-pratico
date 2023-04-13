408563a26ed18e15c67e46863a9f8b8b2f4435d1
pollyanarocha416 <pollyanarocha416@gmail.com>
2023-04-13 10:07:16 -0400

    ecluindo a chave key e jerando ela em ambiente de execucao

--- organiza/settings.py
+++ organiza/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-+qt)#85d%!dtexd3nou0=c9)-(it0#=w&6#z-r6lq4#ebtfln'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
